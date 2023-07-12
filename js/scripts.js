29156b3782880315bad63975d7eecaa37c081c12
German Baena <germanb66@gmail.com>
2023-07-12 15:20:00 -0500

    Actualizando "Cards Modal" en "scripts.js"

--- js/scripts.js
+++ js/scripts.js
@@ -114,7 +114,7 @@ addEventListener('resize', elementHeight)
 // Cards Modal
 cardsModal = document.querySelector('.cards__modal')
 
-// Función abrir Modal.
+// Función abrir Cards Modal.
 document.querySelector('.cards__articles').addEventListener('click', e => {
 
 	e.stopPropagation()
@@ -136,7 +136,7 @@ document.querySelector('.cards__articles').addEventListener('click', e => {
     }
 })
 
-// Función cerrar Modal.
+// Función cerrar Cards Modal.
 cardsModal.addEventListener('click', e => {
     document.querySelector('.cards__template-content').remove()
     cardsModal.setAttribute('style', 'opacity: 0; visibility: hidden' )
