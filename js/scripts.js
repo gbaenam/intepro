72f50f5c51b64cd7c0e166271265ca1645ced4bd
German Baena <germanb66@gmail.com>
2023-08-02 21:38:06 -0500

    Corregido error del Objeto "Expresiones Regulares" en "scripts.js"

--- js/scripts.js
+++ js/scripts.js
@@ -147,7 +147,7 @@ document.querySelector('.cards__footer').addEventListener('click', e => {
 // Objeto Expresiones Regulares.
 const er = {
 	erName: /^(([A-ZÁÉÍÓÚa-zñáéíóú])[\s]?)+$/,
-	erEmail: /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/,f3d3f140d485b5f03a577b7dcdb992d6754df703
+	erEmail: /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/,
 	erTextArea: /^([\w]\s?)([\w\,\.\$\&\#\%\"\¡\!\¿\?\(\)\@\ñ\á\é\í\ó\ú]\s?)+$/
 }
 
